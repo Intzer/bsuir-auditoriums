642bd33e4d5bfee665e474ad77047b68b4b3d21f
roman <rmilh1@mail.ru>
2024-06-29 14:59:54 +0300

    implemented navigation start

--- resources/js/path.js
+++ resources/js/path.js
@@ -33,6 +33,8 @@ const ctx = canvas.getContext('2d');
 
 // Global variables
 let cellSize = 80;
+let startPoint = null;
+let endPoint = null;
 
 // Returning of building choose
 let map = [];
@@ -45,8 +47,9 @@ let endRoom = 0;
 
 // So if we have Map and Rooms we can draw something?
 
+// load values when changing building
 function loadValues(newMap, newRooms) {
-    floor = 1;
+    floor = 1; // Set floor to first
     map = newMap;
     rooms = newRooms;
 
@@ -54,62 +57,37 @@ function loadValues(newMap, newRooms) {
     canvas.width = map[floor][0].length * cellSize;
 }
 
+// Change floor = change canvas size because of different sizes of floors
+// And redraw our map with path
 function changeFloor(value) {
     floor = value;
+    if (floor > map.length) {
+        floor = 1;
+    }
     canvas.height = map[floor].length * cellSize;
     canvas.width = map[floor][0].length * cellSize;
-    drawMapWithPath();
-}
 
-function drawMap() {
-    let text = '';
-    const floorMap = map[floor];
-    const cellSizeX = canvas.width / floorMap[0].length;
-    const cellSizeY = canvas.height / floorMap.length;
+    drawMap();
 
-    for (let y = 0; y < floorMap.length; y++) {
-        for (let x = 0; x < floorMap[0].length; x++) {
-            text = '';
-
-            if (floorMap[y][x] === 'r') {
-                ctx.fillStyle = 'lightblue';
-                rooms.forEach((room) => {
-                    if (room.floor === 1 && room.y === y && room.x === x) {
-                        text = room.name;
-                    }
-                });
-            } else if (floorMap[y][x] === 'c') {
-                ctx.fillStyle = 'white';
-            } else if (floorMap[y][x] === 's') {
-                ctx.fillStyle = 'yellow';
-                text = 'Лестница';
-            } else {
-                ctx.fillStyle = 'gray';
-            }
-
-            ctx.fillRect(x * cellSizeX, y * cellSizeY, cellSizeX, cellSizeY);
-
-            if (text !== '') {
-                ctx.fillStyle = 'black';
-
-                // Измерение ширины текста
-                let textMetrics = ctx.measureText(text);
-                let textWidth = textMetrics.width;
-
-                // Оценка высоты текста
-                let textHeight = parseInt(ctx.font.match(/\d+/), 10);
-
-                // Вычисление позиции для центрирования
-                let textX = x * cellSizeX + Math.round((cellSizeX - textWidth) / 2);
-                let textY = y * cellSizeY + Math.round((cellSizeY + textHeight) / 4);
-
-                ctx.fillText(text, textX, textY);
-            }
+    let path = null;
+    if (floor === endRoom.floor) {
+        path = bfs(findStairs(), endRoom);
+    } else if (floor == startRoom.floor) {
+        if (startRoom.floor === endRoom.floor) {
+            path = bfs(startRoom, endRoom);
+        } else {
+            path = bfs(startRoom, findStairs());
         }
+    } else {
+        path = bfs(findStairs(), findStairs());
     }
+
+    drawPath(path);
 }
 
-function bfs(map, start, end) {
+// Find path from start to end on the map
+function bfs(start, end) {
+    const floorMap = map[floor];
     const directions = [
         [0, 1], [1, 0], [0, -1], [-1, 0]
     ];
@@ -135,7 +113,7 @@ function bfs(map, start, end) {
             const nx = x + dx;
             const ny = y + dy;
 
-            if (nx >= 0 && ny >= 0 && nx < map[0].length && ny < map.length && (map[ny][nx] === 'c' || map[ny][nx] === 'r' || map[ny][nx] === 's')) {
+            if (nx >= 0 && ny >= 0 && nx < floorMap[0].length && ny < floorMap.length && (floorMap[ny][nx] === 'c' || floorMap[ny][nx] === 'r' || floorMap[ny][nx] === 's')) {
                 queue.push([nx, ny, path.concat([[x, y]])]);
             }
         }
@@ -144,25 +122,8 @@ function bfs(map, start, end) {
     return null;
 }
 
-function drawPath(path, cellSizeX, cellSizeY) {
-    if (!path) {
-        return;
-    }
-
-    ctx.strokeStyle = 'red';
-    ctx.lineWidth = 3;
-
-    ctx.beginPath();
-    ctx.moveTo(path[0][0] * cellSizeX + cellSizeX / 2, path[0][1] * cellSizeY + cellSizeY / 2);
-
-    for (let i = 1; i < path.length; i++) {
-        ctx.lineTo(path[i][0] * cellSizeX + cellSizeX / 2, path[i][1] * cellSizeY + cellSizeY / 2);
-    }
-
-    ctx.stroke();
-}
-
 function drawMapWithPath() {
+    floor = 1;
     document.getElementById('path_block').classList.remove('d-none');
 
     const startRoomName = document.getElementById('auditorium_from').value;
@@ -177,7 +138,7 @@ function drawMapWithPath() {
     });
 
     if (!startRoom || !endRoom) {
-        alert('Неверные номера комнат');
+        alert('Неверные номера аудиторий');
         return;
     }
 
@@ -185,20 +146,92 @@ function drawMapWithPath() {
 
     let path = null;
     if (startRoom.floor === endRoom.floor) {
-        path = bfs(map[floor], startRoom, endRoom);
-    } else {
-        const floorMap = map[floor];
-        for (let y = 0; y < floorMap.length; y++) {
-            for (let x = 0; x < floorMap[0].length; x++) {
-                if (floorMap[y][x] === 's') {
-                    endRoom = {x: x, y: y};
-                }
+        path = bfs(startRoom, endRoom);
+    } else if (startRoom.floor > endRoom.floor) { // To downstairs
+        path = bfs(startRoom, findStairs());
+    } else if (startRoom.floor < endRoom.floor) { // to upstairs
+        path = bfs(startRoom, findStairs());
+    }
+    drawPath(path);
+}
+
+function drawMap() {
+    const floorMap = map[floor];
+    const cellSizeX = canvas.width / floorMap[0].length;
+    const cellSizeY = canvas.height / floorMap.length;
+
+    for (let y = 0; y < floorMap.length; y++) {
+        for (let x = 0; x < floorMap[0].length; x++) {
+            let text = '';
+
+            if (floorMap[y][x] === 'r') {
+                ctx.fillStyle = 'lightblue';
+                rooms.forEach((room) => {
+                    if (room.floor === floor && room.y === y && room.x === x) {
+                        text = room.name;
+                    }
+                });
+            } else if (floorMap[y][x] === 'c') {
+                ctx.fillStyle = 'white';
+            } else if (floorMap[y][x] === 's') {
+                ctx.fillStyle = 'yellow';
+                text = 'Лестница';
+            } else {
+                ctx.fillStyle = 'gray';
+            }
+
+            ctx.fillRect(x * cellSizeX, y * cellSizeY, cellSizeX, cellSizeY);
+
+            if (text !== '') {
+                ctx.fillStyle = 'black';
+
+                // Измерение ширины текста
+                let textMetrics = ctx.measureText(text);
+                let textWidth = textMetrics.width;
+
+                // Оценка высоты текста
+                let textHeight = parseInt(ctx.font.match(/\d+/), 10);
+
+                // Вычисление позиции для центрирования
+                let textX = x * cellSize + Math.round((cellSize - textWidth) / 2);
+                let textY = y * cellSize + Math.round((cellSize + textHeight) / 4);
+
+                ctx.fillText(text, textX, textY);
             }
         }
-        path = bfs(map[floor], startRoom, endRoom);
+    }
+}
+
+function drawPath(path) {
+    if (!path) {
+        return;
     }
 
-    drawPath(path, canvas.width / map[floor][0].length, canvas.height / map[floor].length);
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 3;
+
+    ctx.beginPath();
+    ctx.moveTo(path[0][0] * cellSize + cellSize / 2, path[0][1] * cellSize + cellSize / 2);
+
+    for (let i = 1; i < path.length; i++) {
+        ctx.lineTo(path[i][0] * cellSize + cellSize / 2, path[i][1] * cellSize + cellSize / 2);
+    }
+
+    ctx.stroke();
+}
+
+// Find stairs
+function findStairs() {
+    let room = null;
+    const floorMap = map[floor];
+    for (let y = 0; y < floorMap.length; y++) {
+        for (let x = 0; x < floorMap[0].length; x++) {
+            if (floorMap[y][x] === 's') {
+                room = {x: x, y: y};
+            }
+        }
+    }
+    return room;
 }
 
 window.drawMapWithPath = drawMapWithPath;
@@ -220,7 +253,7 @@ loadValues(
             [' ', ' ', 'c', ' ', ' ', 'c', ' ', ' ', ' ', ' '],
             [' ', ' ', 'c', ' ', ' ', 'c', ' ', ' ', ' ', ' '],
             [' ', ' ', 'c', 'c', 'c', 'c', 'c', 'c', 'c', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'r', ' '],
+            [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
         ],
         2: [ // First floor
@@ -238,7 +271,7 @@ loadValues(
     },
     [
         {name: '402', floor: 1, x: 2, y: 1},
-        {name: '409', floor: 2, x: 8, y: 8},
+        {name: '409', floor: 2, x: 5, y: 5},
     ],
 );
 // Mok data end
